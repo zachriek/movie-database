3593b8d800efff1ae78af93ffe890352940f2aac
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2021-06-24 13:24:42 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+function showCards(movie) {
+	return `<div class="col-md-4 my-3">
+				<div class="card">
+					<img src="${movie.Poster}" class="card-img-top">
+					<div class="card-body">
+					    <h5 class="card-title">${movie.Title}</h5>
+					    <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
+					    <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModal" data-imdbid="${movie.imdbID}">Show Details</a>
+					</div>
+				</div>
+			</div>`;
+}
+
+function showMovieDetail(movie) {
+	return `<div class="container-fluid">
+	        	<div class="row">
+	        		<div class="col-md-3">
+	        			<img src="${movie.Poster}" class="img-fluid">
+	        		</div>
+	        		<div class="col-md">
+	        			<ul class="list-group">
+						  <li class="list-group-item"><h4>${movie.Title} (${movie.Year})</h4></li>
+						  <li class="list-group-item"><strong>Director : </strong>${movie.Director}</li>
+						  <li class="list-group-item"><strong>Actors : </strong>${movie.Actors}</li>
+						  <li class="list-group-item"><strong>Writer : </strong>${movie.Writer}</li>
+						  <li class="list-group-item"><strong>Plot : </strong><br>${movie.Plot}</li>
+						</ul>
+	        		</div>
+	        	</div>
+	        </div>`;
+}
+
+$('.search-button').on('click', function() {
+	$.ajax({
+		url: 'http://www.omdbapi.com/?apikey=fa7ef84d&s=' + $('.input-keyword').val(),
+		success: results => {
+			const movies = results.Search;
+			let cards = '';
+			movies.forEach((movie) => {
+				cards += showCards(movie);
+			});
+			$('.movie-container').html(cards);
+			$('.modal-detail-button').on('click', function() {
+				$.ajax({
+					url: 'http://www.omdbapi.com/?apikey=fa7ef84d&i=' + $(this).data('imdbid'),
+					success: (movie) => {
+						const movieDetail = showMovieDetail(movie);
+						$(`.modal-body`).html(movieDetail);
+					},
+					error: (e) => {
+						console.log(e.responseText);
+					}
+				});
+			});
+		},
+		error: e => {
+			console.log(e.responseText);
+		}
+	});
+});
